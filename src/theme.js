8c4f206ddf12b88431ec0aa156c9adc90907bfb5
Charles ELIE-dit-COSAQUE <charles.elie-dit-cosaque@epita.fr>
2025-05-21 10:57:53 +0200

    modification pour garder le theme entre les pages + avoir la radio checked

---
+++ src/theme.js
@@ -0,0 +1,26 @@
+function changeTheme(theme) {
+  document.documentElement.setAttribute('data-theme', theme);
+  localStorage.setItem('theme', theme); // Pour persister le choix de l'utilisateur
+}
+
+// Restaurer le thème au chargement de la page
+document.addEventListener('DOMContentLoaded', () => {
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme) {
+    document.documentElement.setAttribute('data-theme', savedTheme);
+    if (savedTheme === 'retro'){
+      console.log('retro');
+      document.getElementById('theme-retro').checked = true;
+    }
+    else if (savedTheme === 'sunset'){
+      document.getElementById('theme-sunset').checked = true;
+    }
+    else if (savedTheme === 'nord'){
+      document.getElementById('theme-nord').checked = true;
+    }
+  }
+  else {
+    document.getElementById('theme-retro').checked = true;
+  }
+});
+
