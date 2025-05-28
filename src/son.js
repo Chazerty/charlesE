7512b3d190c649b008f8a154893b5f4450a68312
Charles ELIE-dit-COSAQUE <charles.elie-dit-cosaque@epita.fr>
2025-05-28 18:03:46 +0200

    mise en page de la section centre d'interet (ajout de photos, son et script)

--- src/son.js
+++ src/son.js
@@ -1,13 +1,19 @@
 // Précharger le son
-let son;
-        window.onload = function() {
-        son = new Audio('../audio/son_abeille.mp3');
+let son_abeille;
+let kaaris;
+let kaarism;
+let jaxomy;
+window.onload = function() {
+        son_abeille = new Audio('../audio/son_abeille.mp3');
+        kaaris = new Audio('../audio/Kaaris - Zoo.mp3');
+        kaarism = new Audio('../audio/Kaarism - Goulag.mp3');
+        jaxomy = new Audio('../audio/Jaxomy  – Pedro.mp3');
         };
 
 function abeille() {
-    if (son) {
-    son.currentTime = 0;
-    son.play();
+    if (son_abeille) {
+    son_abeille.currentTime = 0;
+    son_abeille.play();
 }};
 
 var audio_troll = document.getElementById('video-troll');
@@ -28,4 +34,73 @@ function pause_troll() {
 function btn_ytb()  {  
     document.getElementById('video_footbar').showModal();
     play_troll()
+}
+
+function play_kaarism() {
+    if (kaarism) {    
+        if (kaarism.paused) {
+            kaarism.play();
+        }
+        else {
+            kaarism.pause();
+    }};
+}
+
+function play_kaaris() {
+    if (kaaris) {
+        if (kaaris.paused) {
+            kaaris.play();
+        }
+        else {
+            kaaris.pause();
+    }};
+}
+
+function play_jaxomy() {
+    if (jaxomy) {
+        if (jaxomy.paused) {
+            jaxomy.play();
+        }
+        else {
+            jaxomy.pause();
+        }
+}};
+
+function reset_kaaris() {
+    if (kaaris) {
+        kaaris.currentTime = 0;
+        kaaris.pause();
+    }
+}
+
+function reset_kaarism() {
+    if (kaarism) {
+        kaarism.currentTime = 0;
+        kaarism.pause();
+    }
+}
+
+function reset_jaxomy() {
+    if (jaxomy) {
+        jaxomy.currentTime = 0;
+        jaxomy.pause();
+    }
+}
+
+if(jaxomy) {
+    jaxomy.addEventListener('ended', function() {
+        reset_jaxomy();
+    });
+}
+
+if(kaarism) {
+    kaarism.addEventListener('ended', function() {
+        reset_kaarism();
+    });
+}
+
+if(kaaris) {
+    kaaris.addEventListener('ended', function() {
+        reset_kaaris();
+    });
 }
